120d7c3a94988bdd5e4bed96a6a828a91468bca7
Lucas Dickmann <131892016+ldickmann@users.noreply.github.com>
2024-12-22 19:11:10 -0300

    Aula: Interagindo com o usuário

--- ReactJS-organo/src/components/InputText/index.js
+++ ReactJS-organo/src/components/InputText/index.js
@@ -1,15 +1,30 @@
 /* Component de input de texto */
 
+import { useState } from "react";
 import "./InputText.css";
 
 const InputText = (props) => {
   // Interpolação de string
   const placeholderModificada = `${props.placeholder}...`;
 
+  // let valor = "Lucas E. Dickmann";
+
+  const [valor, setValor] = useState("");
+
+  const aoDigitar = (event) => {
+    setValor(event.target.value);
+    console.log(valor);
+  };
+
   return (
     <div className="input-text">
       <label>{props.label}</label>
-      <input required={props.required} placeholder={placeholderModificada} />
+      <input
+        value={valor}
+        onChange={aoDigitar}
+        required={props.required}
+        placeholder={placeholderModificada}
+      />
     </div>
   );
 };
