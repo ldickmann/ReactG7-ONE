f7f805632804cc1ad8b9a317874225ea8625712c
Lucas Dickmann <131892016+ldickmann@users.noreply.github.com>
2024-12-26 13:14:27 -0300

    01 - Criando 'Olá Mundo': 04. Alternando entre rotas com JS

--- hello-world-react/src/App.js
+++ hello-world-react/src/App.js
@@ -1,9 +1,14 @@
+import Inicio from "./paginas/Inicio";
+import SobreMim from "./paginas/SobreMim";
+
+// Exemplo de uso do objeto window.location no console do navegador
+console.log(window.location);
+
+// Variável que armazena o componente a ser renderizado, com base na URL
+const pagina = window.location.pathname === "/" ? <Inicio /> : <SobreMim />;
+
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+  return pagina;
 }
 
 export default App;
