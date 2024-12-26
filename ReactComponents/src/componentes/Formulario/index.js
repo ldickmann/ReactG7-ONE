4e5f08d3d937b18e28765caf8c3d571ab9be9c0a
Lucas Dickmann <131892016+ldickmann@users.noreply.github.com>
2024-12-26 07:42:57 -0300

    05 - Finalizando o projeto: 02. Refatorando o formulário

--- ReactComponents/src/componentes/Formulario/index.js
+++ ReactComponents/src/componentes/Formulario/index.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Botao from "../Botao";
-import CampoTexto from "../CampoTexto";
+import Campo from "../Campo";
 import ListaSuspensa from "../ListaSuspensa";
 import "./formulario.css";
 
@@ -27,21 +27,21 @@ const Formulario = ({ aoCadastrar, times, cadastrarTime }) => {
     <section className="formulario-container">
       <form className="formulario" onSubmit={aoSubmeter}>
         <h2>Preencha os dados para criar o card do colaborador.</h2>
-        <CampoTexto
+        <Campo
           obrigatorio={true}
           label="Nome"
           placeholder="Digite seu nome "
           valor={nome}
           aoAlterado={(valor) => setNome(valor)}
         />
-        <CampoTexto
+        <Campo
           obrigatorio={true}
           label="Cargo"
           placeholder="Digite seu cargo "
           valor={cargo}
           aoAlterado={(valor) => setCargo(valor)}
         />
-        <CampoTexto
+        <Campo
           label="Imagem"
           placeholder="Informe o endereço da imagem "
           aoAlterado={(valor) => setImagem(valor)}
@@ -64,7 +64,7 @@ const Formulario = ({ aoCadastrar, times, cadastrarTime }) => {
         }}
       >
         <h2>Preencha os dados para criar um novo time.</h2>
-        <CampoTexto
+        <Campo
           // Já entende que o campo é obrigatório sem passar 'obrigatorio={true}'
           obrigatorio
           label="Nome"
@@ -72,8 +72,9 @@ const Formulario = ({ aoCadastrar, times, cadastrarTime }) => {
           valor={nomeTime}
           aoAlterado={(valor) => setNomeTime(valor)}
         />
-        <CampoTexto
+        <Campo
           obrigatorio
+          type="color"
           label="Cor"
           placeholder="Digite a cor do time"
           valor={corTime}
