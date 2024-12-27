07636bba83d4bd1b6aae7eab44dad1dc3911fd45
Lucas Dickmann <131892016+ldickmann@users.noreply.github.com>
2024-12-27 19:22:02 -0300

    03. Rotas Aninhadas - Concluido

--- ola-mundo/src/paginas/SobreMim/index.js
+++ ola-mundo/src/paginas/SobreMim/index.js
@@ -1,3 +1,17 @@
+import PostModelo from "../../componentes/PostModelo";
+import styles from "./SobreMim.module.css";
+import fotoSobreMim from "../../assets/sobre_mim_foto.png";
+import fotoCapa from "../../assets/sobre_mim_capa.png";
+
 export default function SobreMim() {
-  return <h1>Sobre Mim...</h1>;
+  return (
+    <PostModelo fotoCapa={fotoCapa} titulo="Sobre mim">
+      <h3 className={styles.subtitulo}>Olá, eu sou o Antônio!</h3>
+      <img
+        src={fotoSobreMim}
+        alt="Foto do Antônio Evaldo sorrindo"
+        className={styles.fotoSobreMim}
+      />
+    </PostModelo>
+  );
 }
