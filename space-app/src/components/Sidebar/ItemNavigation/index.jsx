72004f8f31e8c225299b5186a5d772ea7b098b45
Lucas Dickmann <131892016+ldickmann@users.noreply.github.com>
2024-12-29 08:46:24 -0300

    07. Importando as fontes do projeto #1

--- space-app/src/components/Sidebar/ItemNavigation/index.jsx
+++ space-app/src/components/Sidebar/ItemNavigation/index.jsx
@@ -1,6 +1,15 @@
 import { styled } from "styled-components";
 
-const StyledItemNavigation = styled.li``;
+const StyledItemNavigation = styled.li`
+  font-size: 24px;
+  line-height: 29px;
+  margin-bottom: 30px;
+  cursor: pointer;
+  color: ${(props) => (props.$active ? "#7B78E5" : "#D9D9D9")};
+  display: flex;
+  align-items: center;
+  gap: 22px;
+`;
 
 const ItemNavigation = ({
   children,
@@ -9,7 +18,7 @@ const ItemNavigation = ({
   active = false,
 }) => {
   return (
-    <StyledItemNavigation>
+    <StyledItemNavigation $active={active}>
       <img src={active ? activeIcon : inactiveIcon} alt="" />
       {children}
     </StyledItemNavigation>
