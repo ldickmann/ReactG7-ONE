9ee889494ab5b16a1a36bdca0d3646d022708d3d
Lucas Dickmann <131892016+ldickmann@users.noreply.github.com>
2024-12-29 06:35:31 -0300

    05. Normalize CSS

--- space-app/src/App.jsx
+++ space-app/src/App.jsx
@@ -1,4 +1,5 @@
 import { styled } from "styled-components";
+import StylesGlobals from "./components/StylesGlobals";
 
 const FundoGradiente = styled.div`
   background: linear-gradient(
@@ -7,10 +8,16 @@ const FundoGradiente = styled.div`
     #04244f 48%,
     #154580 96.76%
   );
+  width: 100%;
+  min-height: 100vh;
 `;
 
 function App() {
-  return <FundoGradiente></FundoGradiente>;
+  return (
+    <FundoGradiente>
+      <StylesGlobals />
+    </FundoGradiente>
+  );
 }
 
 export default App;
