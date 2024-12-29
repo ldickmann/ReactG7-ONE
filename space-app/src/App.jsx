7980b4e2faeac9403720dde71f73a2f30a8c2340
Lucas Dickmann <131892016+ldickmann@users.noreply.github.com>
2024-12-29 14:23:57 -0300

    04. Estruturando a galeria

--- space-app/src/App.jsx
+++ space-app/src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import Banner from "./components/Banner";
 import bannerImage from "./assets/banner.png";
+import Gallery from "./components/Gallery";
 
 const FundoGradiente = styled.div`
   background: linear-gradient(
@@ -16,16 +17,40 @@ const FundoGradiente = styled.div`
   min-height: 100vh;
 `;
 
+const AppContainer = styled.div`
+  width: 1440px;
+  margin: 0 auto;
+  max-width: 100%;
+`;
+
+const MainContainer = styled.main`
+  display: flex;
+  gap: 24px;
+`;
+
+const GalleryContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+`;
+
 function App() {
   return (
     <FundoGradiente>
       <StylesGlobals />
-      <Header />
-      <Sidebar />
-      <Banner
-        backgroundImage={bannerImage}
-        title="A galeria mais completa de fotos do espaço!"
-      />
+      <AppContainer>
+        <Header />
+        <MainContainer>
+          <Sidebar />
+          <GalleryContent>
+            <Banner
+              backgroundImage={bannerImage}
+              title="A galeria mais completa de fotos do espaço!"
+            />
+            <Gallery />
+          </GalleryContent>
+        </MainContainer>
+      </AppContainer>
     </FundoGradiente>
   );
 }
---
+++ space-app/src/components/Gallery/Popular/index.jsx
@@ -0,0 +1,7 @@
+import Title from "../../Title";
+
+const Popular = () => {
+  return <Title $alignment="center">Populares</Title>;
+};
+
+export default Popular;
