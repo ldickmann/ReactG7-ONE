25a70b15b2e423c7437293b1e5b209852e9b6025
Lucas Dickmann <131892016+ldickmann@users.noreply.github.com>
2024-12-29 02:28:58 -0300

    08 Desafio

--- ola-mundo/src/paginas/Post/index.js
+++ ola-mundo/src/paginas/Post/index.js
@@ -5,6 +5,7 @@ import PostModelo from "../../componentes/PostModelo";
 import ReactMarkdown from "react-markdown";
 import NaoEncontrada from "../NaoEncontrada";
 import PaginaPadrao from "../../componentes/PaginaPadrao";
+import PostCard from "../../componentes/PostCard";
 
 export default function Post() {
   const parametros = useParams();
@@ -16,6 +17,9 @@ export default function Post() {
   if (!post) {
     return <NaoEncontrada />;
   }
+
+  // Filtro para pegar os outros posts que não são o post atual
+  const otherPosts = posts.filter((p) => p.id !== post.id).slice(0, 4);
 
   return (
     <Routes>
@@ -29,6 +33,20 @@ export default function Post() {
             >
               <div className="post-markdown-container">
                 <ReactMarkdown>{post.texto}</ReactMarkdown>
+              </div>
+
+              {/*
+              Desafio - "08. Desafio: implementando posts recomendados"
+              */}
+              <div className="post-recomendados">
+                <h3>Outros posts que você pode gostar:</h3>
+                <ul className="post-recomendados-lista">
+                  {otherPosts.map((otherPosts) => (
+                    <li key={otherPosts.id}>
+                      <PostCard post={otherPosts} />
+                    </li>
+                  ))}
+                </ul>
               </div>
             </PostModelo>
           }
