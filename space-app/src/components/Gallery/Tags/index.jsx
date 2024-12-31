d2fc999b19b61405bb344384348299a6f79f203a
Lucas Dickmann <131892016+ldickmann@users.noreply.github.com>
2024-12-30 23:27:42 -0300

    Desafio: uma tag cheia de estilos

--- space-app/src/components/Gallery/Tags/index.jsx
+++ space-app/src/components/Gallery/Tags/index.jsx
@@ -1,13 +1,44 @@
 import tags from "./tags.json";
+import styled from "styled-components";
+
+const TagsContainer = styled.section`
+  display: flex;
+  align-items: center;
+  gap: 64px;
+  margin-top: 56px;
+`;
+
+const TagTitle = styled.h3`
+  color: #d9d9d9;
+  font-size: 24px;
+  margin: 0;
+`;
+
+const Tag = styled.button`
+  font-size: 24px;
+  color: #ffffff;
+  background: rgba(217, 217, 217, 0.3);
+  border-radius: 10px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+  padding: 12px;
+  box-sizing: border-box;
+  border: 2px solid transparent;
+  &:hover {
+    border-color: #c98cf1;
+  }
+`;
 
 const Tags = () => {
   return (
-    <>
-      <p>Busque por tags:</p>
-      {tags.map((tag) => (
-        <button key={tag.id}>{tag.titulo}</button>
-      ))}
-    </>
+    <TagsContainer>
+      <TagTitle>Busque por tags:</TagTitle>
+      <>
+        {tags.map((tag) => (
+          <Tag key={tag.id}>{tag.titulo}</Tag>
+        ))}
+      </>
+    </TagsContainer>
   );
 };
 
