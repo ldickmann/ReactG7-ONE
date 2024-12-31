7df1f1978ef6d91a1985e0ec356c81c0fc05b8aa
Lucas Dickmann <131892016+ldickmann@users.noreply.github.com>
2024-12-30 23:14:49 -0300

    05. Lidando com tags

--- space-app/src/components/Gallery/Tags/index.jsx
+++ space-app/src/components/Gallery/Tags/index.jsx
@@ -1,5 +1,14 @@
+import tags from "./tags.json";
+
 const Tags = () => {
-  return <p>Tags</p>;
+  return (
+    <>
+      <p>Busque por tags:</p>
+      {tags.map((tag) => (
+        <button key={tag.id}>{tag.titulo}</button>
+      ))}
+    </>
+  );
 };
 
 export default Tags;
