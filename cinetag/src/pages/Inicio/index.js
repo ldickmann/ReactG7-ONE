36c0aa6baec6b7dfdb9472eb3196351234870c6c
Lucas E. Dickmann <131892016+ldickmann@users.noreply.github.com>
2025-01-18 11:43:48 -0300

    06 Lista dinâmica

--- cinetag/src/pages/Inicio/index.js
+++ cinetag/src/pages/Inicio/index.js
@@ -3,6 +3,8 @@ import Cabecalho from "components/Cabecalho";
 import Card from "components/Card";
 import Rodape from "components/Rodape";
 import Titulo from "components/Titulo";
+import videos from "json/db.json";
+import styles from "./Inicio.module.css";
 
 function Inicio() {
   return (
@@ -12,11 +14,11 @@ function Inicio() {
       <Titulo>
         <h1>Um lugar para guardar seus vídeos e filmes!</h1>
       </Titulo>
-      <Card
-        id="1"
-        titulo="Todo Poderoso"
-        capa="https://i.pinimg.com/736x/3e/bd/09/3ebd096bad4640858ae9b9c748b046ea.jpg"
-      />
+      <section className={styles.container}>
+        {videos.map((video) => {
+          return <Card {...video} key={video.id} />;
+        })}
+      </section>
       <Rodape />
     </>
   );
