fbaa7b719288474267ebf285bf109b38a8f4fcea
Lucas E. Dickmann <131892016+ldickmann@users.noreply.github.com>
2025-01-18 14:55:51 -0300

    Finalização aula 02

--- cinetag/src/pages/Inicio/index.js
+++ cinetag/src/pages/Inicio/index.js
@@ -1,18 +1,13 @@
 import Banner from "components/Banner";
-import Cabecalho from "components/Cabecalho";
 import Card from "components/Card";
-import Rodape from "components/Rodape";
 import Titulo from "components/Titulo";
 import videos from "json/db.json";
 
 import styles from "./Inicio.module.css";
 
 function Inicio() {
-  // const { videos, musicas } = db;
-
   return (
     <>
-      <Cabecalho />
       <Banner imagem="home" />
       <Titulo>
         <h1>Um lugar para guardar seus vídeos e filmes!</h1>
@@ -22,17 +17,6 @@ function Inicio() {
           return <Card {...video} key={video.id} />;
         })}
       </section>
-      {/* Exercicio 1
-        
-      <Titulo>
-        <h1>Trilhas Sonoras</h1>
-      </Titulo>
-      <section className={styles.container}>
-        {musicas.map((musica) => {
-          return <Card {...musica} key={musica.id} />;
-        })}
-      </section> */}
-      <Rodape />
     </>
   );
 }
