2c6ccc75760f0a7719fe9f096950f299801065ef
Lucas E. Dickmann <131892016+ldickmann@users.noreply.github.com>
2025-01-18 11:54:46 -0300

    Exercicio 01 - Trilha sonora

--- cinetag/src/pages/Inicio/index.js
+++ cinetag/src/pages/Inicio/index.js
@@ -4,9 +4,12 @@ import Card from "components/Card";
 import Rodape from "components/Rodape";
 import Titulo from "components/Titulo";
 import videos from "json/db.json";
+
 import styles from "./Inicio.module.css";
 
 function Inicio() {
+  // const { videos, musicas } = db;
+
   return (
     <>
       <Cabecalho />
@@ -19,6 +22,16 @@ function Inicio() {
           return <Card {...video} key={video.id} />;
         })}
       </section>
+      {/* Exercicio 1
+        
+      <Titulo>
+        <h1>Trilhas Sonoras</h1>
+      </Titulo>
+      <section className={styles.container}>
+        {musicas.map((musica) => {
+          return <Card {...musica} key={musica.id} />;
+        })}
+      </section> */}
       <Rodape />
     </>
   );
