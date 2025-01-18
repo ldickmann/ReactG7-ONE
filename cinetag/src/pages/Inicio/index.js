4dbd8a05b1a5c714cc43d2ca0d51eff670be2300
Lucas E. Dickmann <131892016+ldickmann@users.noreply.github.com>
2025-01-18 11:36:46 -0300

    04.Cards

--- cinetag/src/pages/Inicio/index.js
+++ cinetag/src/pages/Inicio/index.js
@@ -1,5 +1,6 @@
 import Banner from "components/Banner";
 import Cabecalho from "components/Cabecalho";
+import Card from "components/Card";
 import Rodape from "components/Rodape";
 import Titulo from "components/Titulo";
 
@@ -11,6 +12,11 @@ function Inicio() {
       <Titulo>
         <h1>Um lugar para guardar seus vídeos e filmes!</h1>
       </Titulo>
+      <Card
+        id="1"
+        titulo="Todo Poderoso"
+        capa="https://i.pinimg.com/736x/3e/bd/09/3ebd096bad4640858ae9b9c748b046ea.jpg"
+      />
       <Rodape />
     </>
   );
