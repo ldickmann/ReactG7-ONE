9dad62cf02bc0f80350c680ff8f07ddc5556f372
Lucas E. Dickmann <131892016+ldickmann@users.noreply.github.com>
2025-01-18 15:07:11 -0300

    Desafio: Construindo páginas

--- cinetag/src/pages/Favoritos/index.js
+++ cinetag/src/pages/Favoritos/index.js
@@ -1,7 +1,25 @@
+import Banner from "components/Banner";
+import Titulo from "components/Titulo";
+import Card from "components/Card";
+
 import styles from "./Favoritos.module.css";
 
 function Favoritos() {
-  return <></>;
+  return (
+    <>
+      <Banner imagem="favoritos" />
+      <Titulo>
+        <h1>Meus Favoritos</h1>
+      </Titulo>
+      <section className={styles.container}>
+        <Card
+          id="2"
+          titulo="Todo Poderoso"
+          capa="https://i.pinimg.com/736x/3e/bd/09/3ebd096bad4640858ae9b9c748b046ea.jpg"
+        />
+      </section>
+    </>
+  );
 }
 
 export default Favoritos;
