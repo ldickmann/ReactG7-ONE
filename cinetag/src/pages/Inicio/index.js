c52912fb192c075c16e34febe78fd12d09cb0e4b
Lucas E. Dickmann <131892016+ldickmann@users.noreply.github.com>
2025-01-20 10:09:34 -0300

    05 Fetch com parâmetros

--- cinetag/src/pages/Inicio/index.js
+++ cinetag/src/pages/Inicio/index.js
@@ -1,18 +1,15 @@
 import Banner from "components/Banner";
 import Card from "components/Card";
 import Titulo from "components/Titulo";
-import videos from "json/db.json";
+import { useEffect, useState } from "react";
 
 import styles from "./Inicio.module.css";
-import { useEffect, useState } from "react";
 
 function Inicio() {
   const [videos, setVideos] = useState([]);
 
   useEffect(() => {
-    fetch(
-      "https://my-json-server.typicode.com/ldickmann/cinetag-server/videos"
-    )
+    fetch("https://my-json-server.typicode.com/ldickmann/cinetag-server/videos")
       .then((resposta) => resposta.json())
       .then((dados) => {
         setVideos(dados);
--- cinetag/src/pages/Player/index.js
+++ cinetag/src/pages/Player/index.js
@@ -1,16 +1,24 @@
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Banner from "components/Banner";
 import Titulo from "components/Titulo";
 import NaoEncontrada from "pages/NaoEncontrada";
-import videos from "json/db.json";
 
 import styles from "./Player.module.css";
 
 function Player() {
+  const [video, setVideo] = useState([]);
   const parametros = useParams();
-  const video = videos.find((video) => {
-    return video.id === Number(parametros.id);
-  });
+
+  useEffect(() => {
+    fetch(
+      `https://my-json-server.typicode.com/ldickmann/cinetag-server/videos?id=${parametros.id}`
+    )
+      .then((resposta) => resposta.json())
+      .then((dados) => {
+        setVideo(...dados);
+      });
+  }, [parametros.id]);
 
   if (!video) {
     return <NaoEncontrada />;
